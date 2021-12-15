6d637e6cb5a3d5c37c3cb2aedd782e4b4bc4e864
emerson <69053988+Jed2020@users.noreply.github.com>
2021-12-15 12:14:16 -0300

    install inquirer and changes in the usingGit.js

---
+++ usingGit.js
@@ -0,0 +1,29 @@
+const inquirer = require("inquirer");
+
+
+inquirer
+  .prompt([
+    {
+        type: "list",
+        name: "options",
+        message: "What you will do with Git?!",
+        choices: [{ name: "Start my Git.", value: 0 }, { name: "Status of the my Git.", value: 1 }, { name: "A commit.", value: 2}, { name: "Add a file", value: 3}]
+    }
+  ])
+  .then((responses) => {
+        if (responses.options === 0) {
+            console.log("You need use the command: git init");
+        }
+        else if (responses.channels === 1) {
+            console.log("You need use the command: git status");
+        }
+        else if (responses.channels === 2) {
+            console.log("You need use the command: git commit -m");
+        }
+        else if (responses.channels === 3) {
+            console.log("You need use the command: git add <file>");
+        }
+        else {
+            console.log("Wrong option.");
+        }
+});
